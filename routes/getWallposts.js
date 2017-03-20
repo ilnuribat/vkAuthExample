5236331961f338a2d1a22b9bc56b2aecf3221e97
ilnuribat <ilnuribat@gmail.com>
2017-03-21 01:17:54 +0500

    getting wall posts

--- routes/getWallposts.js
+++ routes/getWallposts.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request');
+const config = require('./../config');
+
+const URL = `https://api.vk.com/method/wall.get?access_token=${config.access_token}`
 
 module.exports = (req, res, next) => {
-    console.log("GET /wallposts");
-    res.send("hello! this is wall posts");
+    request.get(URL + `&owner_id=268041560`, (error, response, body) => {
+        if (error) {
+            res.json(error);
+            return;
+        }
+        res.json(body);
+    });
 }
