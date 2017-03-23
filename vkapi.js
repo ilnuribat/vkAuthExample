230fdb42efe7b58dd655ea09fbee5fec99b13a4a
ilnuribat <ilnuribat@gmail.com>
2017-03-24 01:51:07 +0500

    make general call to vk api via Promise

---
+++ vkapi.js
@@ -0,0 +1,19 @@
+const request = require('request');
+const config = require('./config');
+const access_token = `access_token=${config.access_token}`;
+const URL = `https://api.vk.com/method`;
+
+module.exports = (method, options) =>
+    new Promise((resolve, reject) => {
+        let params = '';
+        for (let key in options)
+            params += `&${key}=${options[key]}`;
+        const URI = `${URL}/${method}?${access_token}${params}`;
+        console.log(URI);
+        request.get(URI, (error, response, body) => {
+            if (error)
+                reject(error);
+            else 
+                resolve(body);
+        });
+    });
