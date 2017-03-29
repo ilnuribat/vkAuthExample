2e614859bfebdf6da40a8c227588401e1368db88
ilnuribat <ilnuribat@gmail.com>
2017-03-30 03:53:14 +0500

    getting list of reposters

--- public/js/repost.js
+++ public/js/repost.js
@@ -1,26 +1,42 @@
+let wallId;
+let postId;
+let linkToPost;
+
 window.onload = () => {
-    const linkToPost = document.getElementById('linkToPost');
+    linkToPost = document.getElementById('linkToPost');
     linkToPost.onchange = () => {
         const extract = extractGroup_IdPost_Id(linkToPost.value);
 
-        const groupId = document.getElementById('groupId');
-        groupId.innerText = extract.groupId;
+        wallId = document.getElementById('wallId');
+        wallId.innerText = extract.wallId;
 
-        const postId = document.getElementById('postId');
+        postId = document.getElementById('postId');
         postId.innerText = extract.postId;
     }
+    const getReposts = document.getElementById('getReposts');
+    getReposts.onclick = APIgetReposts;
 }
 
 function extractGroup_IdPost_Id(linkToPost) {
     let splitted = linkToPost.split('wall');
     if (splitted[0] !== 'https://vk.com/')
         return {
-            groupId: null,
+            wallId: null,
             postId: null
         };
     let ids = splitted[1].split('_');
     return {
-        groupId: ids[0],
+        wallId: ids[0],
         postId: ids[1]
     }
+}
+
+function APIgetReposts() {
+    request('getReposts', {wallId: wallId.innerText, postId: postId.innerText})
+        .then(result => {
+            console.log(result);
+        })
+        .catch(err => {
+            console.log(err);
+        })
 }
