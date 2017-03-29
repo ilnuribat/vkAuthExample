128dba836845f3bc38e56c8a784c39ec75bbfe91
ilnuribat <ilnuribat@gmail.com>
2017-03-29 05:31:24 +0500

    set renderer - pug

--- middleware/index.js
+++ middleware/index.js
@@ -4,6 +4,8 @@ module.exports = (server, express) => {
     server.use(express.static('public'))
     server.use(bodyParser.urlencoded({ extended: true }));
     server.set('json spaces', 4);
+    server.set('views', './views');
+    server.set('view engine', 'pug');
     server.use((req, res, next) => {
         console.log(req.method, req.url);
         next();
