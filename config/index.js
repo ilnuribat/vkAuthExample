736b49a2b2c92955ef381c6c90c04ddd52487b07
ilnuribat <ilnuribat@gmail.com>
2017-03-29 08:02:57 +0500

    added hint for config.json

--- config/index.js
+++ config/index.js
@@ -1,3 +1,11 @@
 const fs = require('fs');
 const config = JSON.parse(fs.readFileSync('./config/config.json'));
 module.exports = config;
+
+/**
+ * config.json must be in this format
+ * {
+ *   "client_id": "appId",
+ *   "access_token": ""
+ *  }
+ */
