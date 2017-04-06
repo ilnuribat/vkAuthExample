07c8dc5cb0a11c9548144efb49edbd849f893ef6
ilnuribat <ilnuribat@gmail.com>
2017-04-06 09:20:54 +0500

    removed old routes

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,9 @@ const router = express.Router();
 const url = require('url');
 
 const auth = require('./auth');
-const getLikes = require('./getLikes.js');
-const getWallPosts = require('./getWallposts');
-const viewsCounter = require('./viewsCounter');
 const getReposts = require('./getReposters');
 
 router.get('/auth', auth);
-router.get('/api/likes', getLikes);
-router.get('/api/wallposts', getWallPosts);
-router.get('/api/viewsCounter', viewsCounter);
 router.get('/api/getReposts', getReposts);
 
 router.get('/login', (req, res) => {
