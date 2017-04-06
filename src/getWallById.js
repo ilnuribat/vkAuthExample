c4efec1347f477bee0f4ef76684fa934ceda6cf2
ilnuribat <ilnuribat@gmail.com>
2017-04-07 01:52:45 +0500

    getting reposts count

---
+++ src/getWallById.js
@@ -0,0 +1,15 @@
+const sql = require('./../sql');
+const vkapi = require('./vkapi');
+
+module.exports = (wallId, postId) =>
+    new Promise((resolve, reject) => {
+        vkapi('wall.getById', {
+            'posts': `${wallId}_${postId}`
+        })
+        .then(res => {
+            resolve(res);
+        })
+        .catch(err => {
+            reject(err);
+        });
+    });
