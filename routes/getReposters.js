714b551607b354332a0a0c089856ead27863a32f
ilnuribat <ilnuribat@gmail.com>
2017-04-06 09:20:44 +0500

    just get 1000 reposters, no more

--- routes/getReposters.js
+++ routes/getReposters.js
@@ -5,24 +5,18 @@ module.exports = (req, res, next) => {
     var params = url.parse(req.url, true).query;
     const wallId = params['wallId'];
     const postId = params['postId'];
-    getReposts(wallId, postId, 0, [], [], [], res);
+    getReposts(wallId, postId, 0, res);
 }
 
-function getReposts(wallId, postId, offset, reposts, groups, profiles, res) {
+function getReposts(wallId, postId, res) {
     vkapi('wall.getReposts', {
         'owner_id': wallId,
         'post_id': postId,
-        'count': 1000,
-        'offset': offset
+        'count': 1000
     })
     .then(body => {
-        if (body.response.items.length > 0)
-            getReposts(wallId, postId, offset + 1000, 
-                body.response.items.concat(reposts), 
-                body.response.groups.concat(groups),
-                body.response.profiles.concat(profiles), res);
-        else 
-            res.json({reposts, groups, profiles});
+        res.json({reposts, groups, profiles});
+        return vkapi('users.get', )
     })
     .catch(error => {
         res.json(error);
