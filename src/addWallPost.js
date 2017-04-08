6625dc7f216e8d75d0ca03078fd75ae0c8fd66e3
ilnuribat <ilnuribat@gmail.com>
2017-04-08 08:21:48 +0500

    adding wallpost and return wallId, postId and repostCount

--- src/addWallPost.js
+++ src/addWallPost.js
@@ -1,8 +1,9 @@
 const sql = require('./../sql');
 //adding wall post to database
 
-const sqlQuery = `INSERT INTO wallposts(wallId, postId, repostsCount)
-                            VALUES($1::int, $2::int, $3::int);`
+const sqlQuery = `INSERT INTO wallposts(wallId, postId, repostsCount, status)
+                            VALUES($1::int, $2::int, $3::int, $4);`
 
 module.exports = (wallId, postId, repostCount) =>
-    sql(sqlQuery, [wallId, postId, repostCount]);
+    sql(sqlQuery, [wallId, postId, repostCount, 'created'])
+    .then(res => ({wallId, postId, repostCount}));
