5e5d9b385b5181c123225e18aef33981da7d0793
ilnuribat <ilnuribat@gmail.com>
2017-04-08 08:21:02 +0500

    added tasks table

--- sql/init.sql
+++ sql/init.sql
@@ -1,9 +1,18 @@
-# DROP TABLE IF EXISTS wallposts;
+DROP TABLE IF EXISTS wallposts;
 CREATE TABLE wallposts (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     wallId INT NOT NULL,
     postId INT NOT NULL,
     status TEXT,
     repostsCount INT NOT NULL,
     UNIQUE (wallId, postId)
+);
+DROP TABLE IF EXISTS tasks;
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    method TEXT NOT NULL,
+    params TEXT NOT NULL,
+    offset_ INT NOT NULL,
+    result JSON,
+    UNIQUE (method, params)
 );
