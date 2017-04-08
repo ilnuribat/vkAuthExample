e7b2bad366770914fbaaff33bad7333d87f9bca2
ilnuribat <ilnuribat@gmail.com>
2017-04-09 01:06:13 +0500

    moved to upper dir

--- src/createRepostsList.js
+++ src/createRepostsList.js
@@ -3,11 +3,11 @@ const method = 'wall.getReposts';
 
 module.exports = ({wallId, postId, repostCount}) => {
   console.log(wallId, postId, repostCount);
-  let sqlQuery = `INSERT INTO tasks(method, params, offset_) VALUES`;
+  let sqlQuery = `INSERT INTO tasks(method, params, offset_, status) VALUES`;
   for (let offset = 0; offset < repostCount; offset += 1000) {
     if (offset !== 0)
       sqlQuery += ', ';//between VALUES()
-    sqlQuery += `('${method}','owner_id=${wallId}&post_id=${postId}&offset=${offset}', ${offset})`;
+    sqlQuery += `('${method}','owner_id=${wallId}&post_id=${postId}&count=1000&offset=${offset}', ${offset}, 0)`;
   }
   sqlQuery += ';';
   return sql(sqlQuery, [])
