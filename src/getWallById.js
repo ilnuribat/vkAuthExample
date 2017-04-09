5ebccfb57c8fa35e54d993b28664f8839cabc18a
ilnuribat <ilnuribat@gmail.com>
2017-04-09 05:05:15 +0500

    removed no-need .then-.catches

--- src/getWallById.js
+++ src/getWallById.js
@@ -2,14 +2,6 @@ const sql = require('./../sql');
 const vkapi = require('./../vkapi');
 
 module.exports = (wallId, postId) =>
-    new Promise((resolve, reject) => {
-        vkapi('wall.getById', {
-            'posts': `${wallId}_${postId}`
-        })
-        .then(res => {
-            resolve(res);
-        })
-        .catch(err => {
-            reject(err);
-        });
+    vkapi('wall.getById', {
+        'posts': `${wallId}_${postId}`
     });
