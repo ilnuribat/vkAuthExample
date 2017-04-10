dfd73935ecd5ff83f6279af60df816ce6b5f037c
ilnuribat <ilnuribat@gmail.com>
2017-04-11 02:16:43 +0500

    make post request

--- vkapi.js
+++ vkapi.js
@@ -1,16 +1,13 @@
 const request = require('request');
 const config = require('./config');
-const access_token = `access_token=${config.access_token}`;
 const URL = `https://api.vk.com/method`;
 
-module.exports = (method, options) =>
+module.exports = (method, form) =>
     new Promise((resolve, reject) => {
-        let params = '';
-        for (let key in options)
-            params += `&${key}=${options[key]}`;
-        const URI = `${URL}/${method}?${access_token}${params}`;
-        console.log(URI);
-        request.get(URI, (error, response, body) => {
+        form['access_token'] = config.access_token;
+        const url = `${URL}/${method}`;
+        console.log(url);
+        request.post({url, form}, (error, response, body) => {
             if (error) {
                 reject(error);
                 return;
