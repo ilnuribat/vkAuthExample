a00c35f3909e4f705bca97a08ced9273f27af499
ilnuribat <ilnuribat@gmail.com>
2017-04-12 19:34:35 +0500

    listening port is moved to middle ware

--- app.js
+++ app.js
@@ -5,10 +5,5 @@ const app = express();
 const middleware = require('./middleware');
 const routes = require('./routes');
 
-middleware(app, express);
 app.use(routes);
-
-
-app.listen(process.env.VK_APP_PORT || 8080, () => {
-    console.log('listening 8080 port');
-});
+middleware(app, express);
