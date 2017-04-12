5f97c7dd3159b90d7a4dcfdb3b8f500ffa1bc117
ilnuribat <ilnuribat@gmail.com>
2017-04-12 19:34:45 +0500

    listening https on 443

--- middleware/index.js
+++ middleware/index.js
@@ -1,4 +1,6 @@
 const bodyParser = require('body-parser');
+const https = require('https');
+const fs = require('fs');
 
 module.exports = (server, express) => {
     server.use(express.static('public'))
@@ -10,4 +12,22 @@ module.exports = (server, express) => {
         console.log(req.method, req.url);
         next();
     });
+
+    const httpPort = process.env.VK_APP_PORT || 8080;
+    server.listen(httpPort, () => {
+        console.log(`listening ${httpPort} port`);
+    });
+    try {
+        const cert = fs.readFileSync('./middleware/cert.pem');
+        const key = fs.readFileSync('./middleware/privkey.pem');
+
+        const httpsServer = https.createServer({cert, key}, server);
+        const httpsPort = 443;
+        httpsServer.listen(httpsPort, () => {
+            console.log(`listening https on ${httpsPort} post`);
+        });
+    }
+    catch(err) {
+        console.log('could not create https server');
+    }
 }
